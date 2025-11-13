93db00eeb16b19436e0b0f3e98f58663edb12222
Milo Canseco <canseco3rd@gmail.com>
2025-11-13 06:07:26 -0700

    Completed the ShowSelection function.:

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,24 @@
 import "./shows.css";
+import PropTypes from "prop-types";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({ shows, setSelectedShow }) {
+  return (
+    <nav>
+      {shows.map((show) => (
+        <a
+          key={show.name}
+          className="show"
+          onClick={() => setSelectedShow(show)}
+        >
+          {show.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
+
+ShowSelection.propTypes = {
+  shows: PropTypes.array.isRequired,
+  setSelectedShow: PropTypes.func.isRequired,
+};
