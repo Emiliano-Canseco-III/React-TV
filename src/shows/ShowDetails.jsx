69884468c514c88936af1276323c7a45cb9d8d46
Milo Canseco <canseco3rd@gmail.com>
2025-11-13 06:13:24 -0700

    Completed ShowDetails function.

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,26 @@
 import "./shows.css";
+import { useState } from "react";
+import PropTypes from "prop-types";
+import EpisodeList from "../episodes/EpisodeList.jsx";
+import EpisodeDetails from "../episodes/EpisodeDetails.jsx";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
+  if (!show) {
+    return <p>Please select a show!</p>;
+  }
+
+  return (
+    <section>
+      <h2>{show.name}</h2>
+      <EpisodeList episodes={show.episodes} onSelect={setSelectedEpisode} />
+      <EpisodeDetails episode={selectedEpisode} />
+    </section>
+  );
 }
+
+ShowDetails.propTypes = {
+  show: PropTypes.object,
+};
