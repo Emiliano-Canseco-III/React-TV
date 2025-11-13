3950b6bef1d567f718758210eddfdb8040d3fd3e
Milo Canseco <canseco3rd@gmail.com>
2025-11-13 06:00:44 -0700

    Imported data from subfolders and added title, showSelection, and ShowDetails.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-/**
- * React TV is an web streaming platform that allows users to browse
- * through the episodes of a variety of different shows.
- */
+import { shows } from "./shows/data.js";
+import { useState } from "react";
+import ShowSelection from "./shows/ShowSelection.jsx";
+import ShowDetails from "./shows/showDetails.jsx";
+
 export default function App() {
+  const [allShows] = useState(shows);
+  const [selectedShow, setSelectedShow] = useState(null);
+
   return (
     <>
       <header>
-        <p>React TV</p>
+        <h1>React TV</h1>
+        <ShowSelection shows={allShows} onSelectedShow={setSelectedShow} />
       </header>
-      <main></main>
+      <main>
+        <ShowDetails show={selectedShow} />
+      </main>
     </>
   );
 }
